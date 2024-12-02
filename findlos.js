518165a88325fe90f001d9dc6ce535a2f63bf943
Jass Khinda <webdeveloperandesigner@gmail.com>
2024-12-01 19:13:25 -0500

    Initial commit for Front End By Meta project

---
+++ findlos.js
@@ -0,0 +1,14 @@
+var age = 199;
+if (age >= 65 && age <= 150)
+     {
+    console.log ("You get your income from your pension");
+}
+else if (age >= 18 && age < 65 )
+{console.log ("Each month you get a salary");
+    }
+    else if (age < 18 && age > 0)
+        console.log ("You get an allowance")
+else {
+console.log("Incorrect Age Record");
+
+}
